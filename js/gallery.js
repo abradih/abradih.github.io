8cbb4f7f404611a9d11af2076494335924faf58a
Amber <abradih@gmail.com>
2019-07-03 19:43:58 -0400

    removed slider and going with something else

--- js/gallery.js
+++ js/gallery.js
@@ -6,7 +6,7 @@ $('.slider').each(function(){
   var buttonArray = [];
   var currentIndex = 0;
   var timeout;
-}
+});
 //move to next
 function advance() {
   clearTimeout(timeout);
@@ -34,3 +34,31 @@ $.each($slides, function(index){
 //move
 advance();
 });
+
+function move(newIndex){
+  var animateLeft, slideLeft;
+  advance();
+
+  if($group.is(':animated') || currentIndex === newIndex){
+    return;
+  }
+
+  buttonArray[currentIndex].removeClass('active');
+  buttonArray[newIndex].addClass('active');
+
+  if(newIndex > currentIndex){
+    slideLeft = '100%';
+    animateLeft = '-100%';
+  }
+  else {
+  slideLeft='-100%';
+  animateLeft='100%';
+  }
+  $slides.eq(newIndex).css({left:slideLeft, display: 'block'});
+  $group.animate({left:animateLeft}, function(){
+    $slides.eq(currentIndex).css({display:'none'});
+    $slides.eq(newIndex).css({left:0});
+    $group.css({left:0});
+    currentIndex = newIndex;
+  });
+}
